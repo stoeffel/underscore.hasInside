a1642f5d4778666f2090649d7b7b5825497c392b
Christoph Hermann <schtoeffel@gmail.com>
2014-06-12 12:16:38 +0200

    exposes findInside

--- README.md
+++ README.md
@@ -1,22 +1,45 @@
 underscore.hasInside
 ====================
 
-Check if an object or it's descendants have a property.
+Check if an object or it's descendants fullfill a predicate
 
 Installation
 ------------
 
 `npm install underscore.has-inside`
 
+
 Usage
 -----
 
+### setup
+
 ```javascript
-        _.mixin({
-            hasInside: require('underscore.has-inside')
-        });
+        _.mixin(require('underscore.has-inside'));
+```
 
 
+### findInside
+
+Checks a predicate for an object and its descendant
+```javascript
+        var obj = {
+            foo: 'bar',
+            moo: {
+                boo: 'poo'
+            },
+            arrrr: [1,2,3,4]
+        };
+        _.findInside(obj, function(item) {
+            return item.boo && item.boo === 'poo';
+        });  // -> true
+```
+
+
+### hasInside
+
+Check if an object or it's descendants have a property
+```javascript
         var obj = {
             foo: 'bar',
             moo: {
--- test/hasInside.js
+++ test/hasInside.js
@@ -4,47 +4,79 @@ var should = require('should'),
 
 describe('hasInside', function() {
     beforeEach(function() {
-        _.mixin({
-            hasInside: hasInside
+        _.mixin(hasInside);
+    });
+
+    describe('#findInside', function() {
+        it('should return false if not an object is passed', function() {
+            _.findInside().should.not.be.ok;
+            _.findInside([], function() {}).should.not.be.ok;
+            _.findInside(1).should.not.be.ok;
+            _.findInside('foo').should.not.be.ok;
+        });
+
+        it('should return false if an object but no predicate is passed', function() {
+            _.findInside({}).should.not.be.ok;
+        });
+
+        it('should evaluate the predicate for each object', function() {
+            var predicate = function(obj) {
+                return obj.foo && obj.foo === 'bar';
+            };
+            _.findInside({
+                foo: 'bar'            
+            }, predicate).should.be.ok;
+
+            _.findInside({
+                moo: 'boo'            
+            }, predicate).should.not.be.ok;
+
+            _.findInside({
+                sub: {
+                    moo: 'boo'            
+                }
+            }, predicate).should.not.be.ok;
         });
     });
 
-    it('should return false if not an object is passed', function() {
-        _.hasInside().should.not.be.ok;
-        _.hasInside([], 'bar').should.not.be.ok;
-        _.hasInside(1, 'bar').should.not.be.ok;
-        _.hasInside('foo', 'bar').should.not.be.ok;
-        _.hasInside({}).should.not.be.ok;
-    });
+    describe('#hasInside', function() {
+        it('should return false if not an object is passed', function() {
+            _.hasInside().should.not.be.ok;
+            _.hasInside([], 'bar').should.not.be.ok;
+            _.hasInside(1, 'bar').should.not.be.ok;
+            _.hasInside('foo', 'bar').should.not.be.ok;
+            _.hasInside({}).should.not.be.ok;
+        });
 
-    it('should return false if object has no property bar', function() {
-        _.hasInside({
-            foo: 'bar'
-        }, 'bar').should.not.be.ok;
-    });
+        it('should return false if object has no property bar', function() {
+            _.hasInside({
+                foo: 'bar'
+            }, 'bar').should.not.be.ok;
+        });
 
-    it('should return true if object has property foo', function() {
-        _.hasInside({
-            foo: 'bar'
-        }, 'foo').should.be.ok;
-    });
+        it('should return true if object has property foo', function() {
+            _.hasInside({
+                foo: 'bar'
+            }, 'foo').should.be.ok;
+        });
 
-    it('should return false if object and descendants have no property bar', function() {
-        _.hasInside({
-            foo: 'bar',
-            moo: {
-                boo: 'poo'
-            }
-        }, 'bar').should.not.be.ok;
-    });
+        it('should return false if object and descendants have no property bar', function() {
+            _.hasInside({
+                foo: 'bar',
+                moo: {
+                    boo: 'poo'
+                }
+            }, 'bar').should.not.be.ok;
+        });
 
-    it('should return true if descendants has property boo', function() {
-        _.hasInside({
-            foo: 'bar',
-            moo: {
-                boo: 'poo'
-            },
-            arrrr: [1, 2, 3, 4]
-        }, 'boo').should.be.ok;
+        it('should return true if descendants has property boo', function() {
+            _.hasInside({
+                foo: 'bar',
+                moo: {
+                    boo: 'poo'
+                },
+                arrrr: [1, 2, 3, 4]
+            }, 'boo').should.be.ok;
+        });
     });
 });
